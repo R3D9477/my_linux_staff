7e7c2b88851ae1e273490eaaf2950b4498202a8a
r3d9u11 <r3d9u11@gmail.com>
2020-02-13 17:53:46 +0300

    Update plasma_fix_after_suspending.sh

--- helpers_workarounds/plasma_fix_after_suspending.sh
+++ helpers_workarounds/plasma_fix_after_suspending.sh
@@ -10,9 +10,6 @@ function fix_plasma() {
         /opt/viber/Viber %U &
     fi
 
-    #/usr/bin/kstart5 -- plasmashell --replace      # --- I don't recommend to use it at startup time, because somethimes it hangs
-    #/usr/bin/kquitapp5 plasmashell                 # --- I also don't recommend to use it at startup time, because somethimes it hangs, too
-
     /usr/bin/killall -SIGKILL plasmashell
     /usr/bin/kstart5 -- plasmashell --replace
 
