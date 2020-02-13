82516fbd06418387b92877173b6ce906ecaa94f5
r3d9u11 <r3d9u11@gmail.com>
2020-02-13 16:28:43 +0300

    Update plasma_fix_after_suspending.sh

--- helpers_workarounds/plasma_fix_after_suspending.sh
+++ helpers_workarounds/plasma_fix_after_suspending.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 function fix_plasma() {
-    if [ -f "/usr/bin/latte-dock" ]; then
-        /usr/bin/latte-dock --replace &
+    if [ ! -z "$(pidof Viber)" ]; then
+        /usr/bin/kstart5 -- latte-dock --replace
     fi
 
-    if [ -f "/opt/viber/Viber" ]; then
-        killall -SIGKILL Viber
+    if [ ! -z "$(pidof latte-dock)" ]; then
+        /usr/bin/killall -SIGKILL Viber
         /opt/viber/Viber %U &
     fi
 
     #/usr/bin/kstart5 -- plasmashell --replace      # --- I don't recommend to use it at startup time, because somethimes it hangs
     #/usr/bin/kquitapp5 plasmashell                 # --- I also don't recommend to use it at startup time, because somethimes it hangs, too
 
-    killall -SIGKILL plasmashell
+    /usr/bin/killall -SIGKILL plasmashell
     /usr/bin/kstart5 -- plasmashell --replace
 
     /usr/bin/kstart5 -- kwin --replace
