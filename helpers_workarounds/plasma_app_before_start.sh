2da61eefe2af681add79f495e5507d7917a5aa6e
r3d9u11 <r3d9u11@gmail.com>
2020-03-02 22:40:05 +0300

    kwin restart

--- helpers_workarounds/plasma_app_before_start.sh
+++ helpers_workarounds/plasma_app_before_start.sh
@@ -5,10 +5,20 @@ qdbus org.kde.keyboard /Layouts setLayout "us"
 
 function fix_suspended_compositor() {
 
-    sleep 3s # wait 3s until target application will not be launched
-    kstart5 -- kwin --replace
+    if [ ! -f "/tmp/plasma_fix.lock" ] ; then
+        echo 1 > "/tmp/plasma_fix.lock"
+
+        # wait 10s until target application will not be launched
+        # and compositor will not be suspended on demand
+        sleep 10s
+
+        if ! $(qdbus org.kde.KWin /Compositor active) ; then
+            kstart5 -- kwin --replace
+            sleep 1s # wait 1s until KWin will not be loaded
+        fi
+
+        rm "/tmp/plasma_fix.lock"
+    fi
 }
 
-if ! $(qdbus org.kde.KWin /Compositor active) ; then
-    fix_suspended_compositor & # restart KWin if compositor is suspended to avoid it's freezing
-fi
+fix_suspended_compositor & # restart KWin if compositor is suspended to avoid it's freezing
